b3106706243966351473bf245d52c414be30b911
Talha-1010 <talhanasr3e@gmail.com>
2021-12-15 21:52:15 +0500

    card title issue resolved

--- src/card.js
+++ src/card.js
@@ -14,7 +14,7 @@ function Card(props) {
                 <div class="card" style={{width: "18rem"}}>
                     <img src="https://vistapointe.net/images/hostel-7.jpg" class="card-img-top" alt="..."/>
                     <div class="card-body">
-                        <h5 class="card-title">{props.title}</h5>
+                        <h5 class="card-title">{props.hostelName}</h5>
                         <p class="card-text">{props.address}</p>
                         <Link  to= { {pathname: '/roomsAndBeds', state:props } }  class="btn btn-primary">Go somewhere</Link>
                         {/*  */}
